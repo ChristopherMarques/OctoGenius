9b877ae7d24f8a68706f9b7f6bc39756bf90bd8d
ChristopherMarques <chriscorrea.marques@gmail.com>
2025-03-08 02:13:51 -0300

    update colors on intention-on component

--- src/components/diagnostic/intention-on.tsx
+++ src/components/diagnostic/intention-on.tsx
@@ -1,21 +1,33 @@
 import { Vestibulares } from "@/lib/utils/constants";
 import { Select, SelectItem, SelectContent, SelectTrigger, SelectValue } from "../ui/select";
 import { Label } from "../ui/label";
+import { useTheme } from "next-themes";
+import { cn } from "@/lib/design-utils";
 
 interface IntentionOnProps {
     value: string;
     onChange: (value: string) => void;
 }
 
-export const IntentionOn = ({ value, onChange }: IntentionOnProps) => (
-    <Select onValueChange={onChange} value={value}>
-        <SelectTrigger className="bg-white my-4">
-            <SelectValue placeholder="Selecione um vestibular" />
-        </SelectTrigger>
-        <SelectContent className="max-h-[300px] overflow-y-auto bg-white z-50 opacity-100">
-            {Vestibulares.map((vestibular) => (
-                <SelectItem key={vestibular} value={vestibular}>{vestibular}</SelectItem>
-            ))}
-        </SelectContent>
-    </Select>
-);
+export const IntentionOn = ({ value, onChange }: IntentionOnProps) => {
+    const { theme } = useTheme();
+    const isDarkMode = theme === "dark";
+
+    const className = cn(
+        "max-h-[300px] overflow-y-auto bg-[black || white] z-50 ",
+        isDarkMode ? "bg-black" : "bg-white"
+    );
+    return (
+
+        <Select onValueChange={onChange} value={value}>
+            <SelectTrigger className="bg-background my-4">
+                <SelectValue placeholder="Selecione um vestibular" />
+            </SelectTrigger>
+            <SelectContent className={className}>
+                {Vestibulares.map((vestibular) => (
+                    <SelectItem key={vestibular} value={vestibular}>{vestibular}</SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    )
+};
