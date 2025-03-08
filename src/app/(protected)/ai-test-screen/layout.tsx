a8d816ff15dd66a459e710e31680ab197d0d4200
ChristopherMarques <chriscorrea.marques@gmail.com>
2025-03-08 02:02:24 -0300

    deleted ai test screen

--- src/app/(protected)/ai-test-screen/layout.tsx
+++
@@ -1,6 +0,0 @@
-const AiTestScreenLayout = ({ children }: { children: React.ReactNode }) => {
-    return <div className="flex flex-col h-screen w-screen justify-center items-center">{children}</div>;
-};
-
-export default AiTestScreenLayout;
-
