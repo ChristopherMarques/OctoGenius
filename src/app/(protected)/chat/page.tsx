888b1ba427e00d21032149c2d0e7cb788113d792
ChristopherMarques <chriscorrea.marques@gmail.com>
2025-07-25 18:48:25 -0300

    added diagnostic

--- src/app/(protected)/chat/page.tsx
+++ src/app/(protected)/chat/page.tsx
@@ -1,11 +1,9 @@
-import { ChatWidget } from "@/components/chat/chat-widget"
 import WorkingChatbot from "@/components/mvpblocks/working-chatbot"
 
 export default function ChatPage() {
     return (
         <div className="flex h-svh py-2 flex-col">
             <WorkingChatbot />
-            <ChatWidget />
         </div>
     )
 }
